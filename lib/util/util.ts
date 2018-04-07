be6470599c93e469b7d9b5664fa76c85e7ec137b
roman <shmoop207@gmail.com>
2018-04-07 21:37:35 +0300

    first

--- lib/util/util.ts
+++ lib/util/util.ts
@@ -2,34 +2,11 @@
 
 import _ = require('lodash');
 import fs = require('fs');
-import {Injector, createContainer} from "appolo-inject";
+import {Injector, Util as InjectUtil} from "appolo-inject";
 
-export class Util {
-    public static getClassName(fn: Function): string {
-        return fn.name.charAt(0).toLowerCase() + fn.name.slice(1)
-    }
-
-
-    public static getFunctionArgs(func: (...args: any[]) => any) {
-
-        const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-        const ARGUMENT_NAMES = /([^\s,]+)/g;
-
-        let fnStr = func.toString().replace(STRIP_COMMENTS, '');
-        let args = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
-
-        if (args === null) {
-            args = [];
-        }
-
-        args = _.compact(args);
-
-        return args;
-    }
-
+export class Util extends InjectUtil {
 
     public static async loadPathWithArgs(paths: string[], injector: Injector) {
-        //let modulesPath = path.join(this._options.root, 'config/modules/modules.js');
 
         for (let path of paths) {
             if (!fs.existsSync(path)) {
@@ -56,28 +33,6 @@ export class Util {
     }
 
 
-    // public static  namespace(namespace: string, value: any) {
-    //     let properties = namespace.split('.');
-    //
-    //     let parent = global;
-    //
-    //     while (properties.length) {
-    //         let property = properties.shift();
-    //         if (typeof parent[property] === 'undefined') {
-    //             parent[property] = {};
-    //         }
-    //
-    //         if (properties.length == 0 && value) {
-    //             parent[property] = value;
-    //         }
-    //         else {
-    //             parent = parent[property];
-    //         }
-    //     }
-    //
-    //     return parent;
-    // }
-
     public static mixins(_klass: Function, mixins: Function | Function[]) {
 
         _.forEach(_.isArray(mixins) ? mixins : [mixins], (mixin) => {
@@ -88,44 +43,6 @@ export class Util {
         });
 
     }
-
-    // public static  statics(_klass:Function,key:{[index:string]:any}|string, value?:any) {
-    //
-    //
-    //     let statics = {};
-    //
-    //     if (value) {
-    //         statics[key as string] = value
-    //     } else {
-    //         statics = key;
-    //     }
-    //
-    //     _.forEach(statics, (func, name) => {
-    //
-    //         Object.defineProperty(_klass, name, {
-    //             get: function () {
-    //                 return func
-    //             }
-    //         });
-    //
-    //         Object.defineProperty(_klass.prototype, name, {
-    //             get: function () {
-    //                 return func
-    //             }
-    //         });
-    //     });
-    //
-    // }
-    //
-    // public static cloneArr<T>(a: T[]): T[] {
-    //     let b = new Array(a.length);
-    //     let i = a.length;
-    //     while (i--) {
-    //         b[i] = a[i];
-    //     }
-    //
-    //     return b;
-    // }
 
 
 }
