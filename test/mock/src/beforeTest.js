cf3f0025c96f8f453cba4961485e98ca8d11f188
roman <shmoop207@gmail.com>
2019-02-18 18:48:38 +0200

    added before and after decorators

---
+++ test/mock/src/beforeTest.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const index_1 = require("../../../index");
+let BeforeTest = class BeforeTest {
+    run(num) {
+        return num + 5;
+    }
+    runAfter(num) {
+        return num + 5;
+    }
+};
+BeforeTest = tslib_1.__decorate([
+    index_1.define()
+], BeforeTest);
+exports.BeforeTest = BeforeTest;
+//# sourceMappingURL=beforeTest.js.map
