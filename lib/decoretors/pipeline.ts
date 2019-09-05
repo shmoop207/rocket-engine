6d7d9a9815f89ba1305f05944201ec6208b9dfdd
roman <shmoop207@gmail.com>
2019-09-05 17:49:50 +0300

    fix pipeline inherit

--- lib/decoretors/pipeline.ts
+++ lib/decoretors/pipeline.ts
@@ -8,7 +8,7 @@ export const PipeSetSymbol = "__PipeSetSymbol__";
 export function pipeline(pipeline: IPipelineFn | IPipelineCtr, metaData?: any, options?: any) {
 
     return function (target: any, propertyKey: string, descriptor?: PropertyDescriptor | number) {
-        let result: IMetadata = Reflector.getOwnMetadata(PipeSymbol, target.constructor, undefined, {});
+        let result: IMetadata = Reflector.getMetadata(PipeSymbol, target.constructor, undefined, {});
 
         if (!result[propertyKey]) {
             result[propertyKey] = [];
