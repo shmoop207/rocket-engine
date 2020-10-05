f962b7410e24ae13448d6f76798fee02d90f53cb
roman <shmoop207@gmail.com>
2020-10-05 21:50:01 +0300

    refactor module events

---
+++ test/mock/config/modules/testLoad/src/test.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Test = void 0;
+const tslib_1 = require("tslib");
+const inject_1 = require("@appolo/inject");
+let Test = class Test {
+    initialize() {
+        this.rootEnv.testLoadModule = true;
+    }
+    get name() {
+        return "working";
+    }
+};
+tslib_1.__decorate([
+    inject_1.inject(),
+    tslib_1.__metadata("design:type", Object)
+], Test.prototype, "rootEnv", void 0);
+tslib_1.__decorate([
+    inject_1.initMethod(),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", []),
+    tslib_1.__metadata("design:returntype", void 0)
+], Test.prototype, "initialize", null);
+Test = tslib_1.__decorate([
+    inject_1.define(),
+    inject_1.singleton()
+], Test);
+exports.Test = Test;
+//# sourceMappingURL=test.js.map
---
+++ test/mock/config/modules/testLoad/testLoadModule.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TestLoadModule = void 0;
+const tslib_1 = require("tslib");
+const index_1 = require("../../../../../index");
+let TestLoadModule = class TestLoadModule extends index_1.Module {
+    get exports() {
+        return [];
+    }
+};
+TestLoadModule = tslib_1.__decorate([
+    index_1.module({})
+], TestLoadModule);
+exports.TestLoadModule = TestLoadModule;
+//# sourceMappingURL=testLoadModule.js.map
